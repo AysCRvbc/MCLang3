240e43671a3c2aee831b676651f8094618793c6c
Cludi <cyrsiaesixd@gmail.com>
2024-03-05 23:56:54 +0400

    крутые правки в скалу + переменные

--- fastcmd/modules/pass_expression.py
+++ fastcmd/modules/pass_expression.py
@@ -1,13 +1,14 @@
 import mclang.syntax.PrcParser as prc
 import mclang.parser as pr
 from mclang.namespace import Namespace
-import mclang.syntax.expressions.Selector as sct
 
 
 class Parser(prc.PrcParser):
     def getName(self):
-        return None
+        return "pass"
     def parse(self, block, meta, base=None, data=None):
         nmeta = meta["NMETA"]
         ns: Namespace = meta["NMETA"].getNamespace()
         parser: pr.CodeParser = meta["PARSER"]
+
+        return []
--- mclang/syntax/expressions/Variable.py
+++ mclang/syntax/expressions/Variable.py
@@ -1,8 +1,12 @@
 import mclang.syntax.PrcParser as Prc
 from mclang.namespace import Namespace
+import mclang.syntax.expressions.Selector as sct
 
 
 class Parser(Prc.PrcParser):
+    def __init__(self):
+        self.name = None
+
     def parse(self, block, meta, base=None, data=None):
         block = block.split()
         ns: Namespace = meta["NMETA"].getNamespace()
@@ -11,3 +15,23 @@ class Parser(Prc.PrcParser):
             valtype = block[1]
 
         ns.setValue(block[0], "scoreboard", meta=valtype)
+        ns.getValue(block[0])["pointer"] = self
+
+        self.name = block[0]
+
+
+    def store(self, args, meta):
+        ns: Namespace = meta["NMETA"].getNamespace()
+
+        command = args[0]
+        res_type = "result"
+        if len(args) > 1:
+            res_type = args[1]
+        selector = "@s"
+        if len(args) > 2:
+            selector = args[2]
+        selector = sct.getSelector(selector, meta)
+
+        cmd = f"execute store {res_type} score {selector} {ns.getValue(self.name)['value']} run {command}"
+
+        return [{"type": "command", "value": cmd}]
